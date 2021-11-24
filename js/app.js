727ef79f46f0dfcf13ec9c02b42abc013c67199e
Certified Genius <mugeera.mansoor@medieinstitutet.se>
2021-11-24 13:47:36 +0100

    Disney

--- js/app.js
+++ js/app.js
@@ -5,12 +5,10 @@ fetch('https://fakestoreapi.com/products')
 }).then((completedata) => {
     let data = "";
     completedata.map((values) => {
-        data += `<div class="card">
+        data += `<div id="${values.id}" class="card"> 
         <h1 class="title">${values.title}</h1>
         <img src="${values.image}" alt="img" class="images">
-        <p>${values.description}</p>
-        <p class="category">${values.category}</p>
-        <p class="price">${values.price}</p>
+        <p class="price">${values.price} kr</p>
     </div>`
     });
     document.getElementById("cards").innerHTML = data;
