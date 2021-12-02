bfc6e687be86c36d5d447bd2a70af19f9807ed40
Certified Genius <mugeera.mansoor@medieinstitutet.se>
2021-12-02 17:19:37 +0100

    card move

--- js/Price-Filter.js
+++ js/Price-Filter.js
@@ -1,67 +1,34 @@
-// Fetch Catogory from json instead of data-category="" data-price="" in html
-(function () {
-
-    let field = document.getElementById('.cards');
-    let li = Array.from(field.children); // WHAT IS FIELD CHILDREN?
+// Allproducts box, with an Array inside
+let allProducts = [];
 
 
-    fetch('/data/data.json')
-        .then(response => response.json())
-        .then(json => {
-            
-            // Price Filter
-            function SortProduct() {
-                let select = document.getElementById('select');
-                let ar = [];
-                
-                for (let i of json) {
-                    const last = i.lastElementChild;
-                    const x = last.textContent.trim();
-                    const y = Number(x.substring(1));
-                    i.setAttribute("data-price", y); // Tabort denna?
-                    ar.push(i);
-                }
+// Fetch Catogory from json 
+fetch("/data/data.json")
+    
+    .then((data) => {
+        return data.json();
 
-                this.run = () => {
-                    addevent();
-                }
-
-                function addevent() {
-                    select.onchange = sortingValue;
-                }
-
-                function sortingValue() {
-                    if (this.value === 'Default') {
-                        while (field.firstChild) {
-                            field.removeChild(field.firstChild);
-                        }
-                        field.append(...ar);
-                    }
-                    if (this.value === 'LowToHigh') {
-                        SortElem(field, li, true)
-                    }
-                    if (this.value === 'HighToLow') {
-                        SortElem(field, li, false)
-                    }
-                }
-
-                function SortElem(field, li, asc) {
-                    let dm, sortli;
-                    dm = asc ? 1 : -1;
-                    sortli = li.sort((a, b) => {
-                        const ax = a.getAttribute('data-price'); // Tabort denna?
-                        const bx = b.getAttribute('data-price'); // Tabort denna?
-                        return ax > bx ? (1 * dm) : (-1 * dm);
-                    });
-                    while (field.firstChild) {
-                        field.removeChild(field.firstChild);
-                    }
-                    field.append(...sortli);
-                }
-            }
-
-            new SortProduct().run();
-        });
+    })
+    .catch((error) => console.log(error));
 
 
-})();
+
+
+
+// Sort By Price
+function sortByPriceFilter() {
+    let select = document.getElementById("select").value;
+
+
+    if (select === "Lowest-Price") {
+        let sorted = allProducts.slice().sort((a, b) => a.price - b.price);
+    } 
+    
+    
+    else if (select === "Highest-Price") {
+        let sorted = allProducts.slice().sort((a, b) => a.price - b.price);
+    }
+
+    
+    else (select === "Default")
+}
